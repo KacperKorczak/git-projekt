ec40e5d92e82a1b64c93ededbe8eb3768934a9b5
Kacper Korczak <kacper.korczak123@gmail.com>
2021-03-18 00:06:02 +0100

    s

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ echo "Data i czas: $data ";
 $servername = "localhost";
 $username = "user";
 $password = "a";
-$dbname = "user";
+$dbname = "user"; 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
